3cdc843c363d15100425b77e76a157c893448f31
plus <lipho@plus20charisma.com>
2016-06-18 18:17:29 -0700

    update

--- ngapp.js
+++ ngapp.js
@@ -3,6 +3,13 @@ require('angular-animate')
 
 var app = angular.module('clg', ['ngAnimate']);
 
+app.filter('capitalize', function() {
+	return function(input, all) {
+		var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+		return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+	}
+});
+
 app.controller('formCtrl', ['$scope', function($scope){
 	
 	$scope.date = new Date();
@@ -30,8 +37,6 @@ app.controller('formCtrl', ['$scope', function($scope){
 		this.resetJob()
 		this.resetCurrent();
 	};
-
-	
 
 }]);
 
--- public/javascripts/ng.js
+++ public/javascripts/ng.js
@@ -4,6 +4,13 @@ require('angular-animate')
 
 var app = angular.module('clg', ['ngAnimate']);
 
+app.filter('capitalize', function() {
+	return function(input, all) {
+		var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+		return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+	}
+});
+
 app.controller('formCtrl', ['$scope', function($scope){
 	
 	$scope.date = new Date();
@@ -31,8 +38,6 @@ app.controller('formCtrl', ['$scope', function($scope){
 		this.resetJob()
 		this.resetCurrent();
 	};
-
-	
 
 }]);
 
